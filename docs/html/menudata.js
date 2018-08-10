e462e4a8b52b7e233c5aa904645e939fa840b909
steve <h1liang@ucsd.edu>
2018-08-10 13:05:40 -0700

    added comments for handleTC()

---
+++ docs/html/menudata.js
@@ -0,0 +1,11 @@
+var menudata={children:[
+{text:"Home",url:"index.html"},
+{text:"Manual",url:"manual.html",children:[
+{text:"Startup Summary",url:"startup.html"},
+{text:"Making Serial Connection",url:"serial.html"},
+{text:"Making Ethernet Connection",url:"ethernet.html"},
+{text:"Making WIFI Connection",url:"wifi.html"}]},
+{text:"API Modules",url:"modules.html"},
+{text:"Examples",url:"examples.html"},
+{text:"GitHub",url:"https://github.com/haaseresearch/code_roc2_git"},
+{text:"Issue Tracker",url:"https://github.com/haaseresearch/code_roc2_git/issues"}]}
---
+++ docs/html/modules.js
@@ -0,0 +1,8 @@
+var modules =
+[
+    [ "Deprecated", "group___deprecated.html", null ],
+    [ "Math", "group___math.html", null ],
+    [ "IO", "group___i_o.html", null ],
+    [ "Mavlink", "group___mavlink.html", null ],
+    [ "Quadrature_Encoder", "group___quadrature___encoder.html", null ]
+];
